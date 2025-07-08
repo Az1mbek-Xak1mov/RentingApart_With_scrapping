acbef3522d5b43323ba0e14e1fe3db899e657674
Az1mbek-Xak1mov <azimbekhakimov2@gmail.com>
2025-07-08 14:27:58 +0500

    Update webscrape

--- webscrape/scrapping_urls_olx.py
+++ webscrape/scrapping_urls_olx.py
@@ -7,7 +7,9 @@ from webscrape.process_olx import process_olx_ad
 session = SessionLocal()
 def get_all_urls_for_apart(filter_url:str):
     seen_urls = set()
+    print(1)
     for page in range(1, 3):
+        print(2)
         url = (
             f"{filter_url}&page={page}"
         )
@@ -19,6 +21,7 @@ def get_all_urls_for_apart(filter_url:str):
         soup = BeautifulSoup(resp.text, "html.parser")
 
         for a in soup.find_all("a", class_="css-1tqlkj0"):
+            print(3)
             href = a.get("href")
             if not href:
                 continue
@@ -27,14 +30,17 @@ def get_all_urls_for_apart(filter_url:str):
 
             # Skip if we've already added it in this run:
             if full_url in seen_urls:
+                print(4)
                 continue
 
             exists = session.query(
                 session.query(ApartmentUrl).filter_by(url=full_url).exists()
             ).scalar()
             if exists:
+                print(5)
                 continue
             seen_urls.add(full_url)
+            print(6)
             session.add(ApartmentUrl(url=full_url, status="new"))
     session.commit()
     session.close()
